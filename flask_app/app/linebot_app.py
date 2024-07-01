99e4fd9299ce7156a9d2bcd8a0c9f9bd2432b6aa
Shungo Ikai <ikai.shungo@gmail.com>
2024-07-01 22:28:13 +0900

    応答の動作確認が取れた状態

--- flask_app/app/linebot_app.py
+++ flask_app/app/linebot_app.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import os
 from flask import Flask, abort, request, jsonify
 from dotenv import load_dotenv
+from linebot import (
+    LineBotApi
+)
 from linebot.v3.webhook import (
     WebhookHandler
 )
@@ -34,12 +37,17 @@ load_dotenv(str(ENV_DIR))
 app = Flask(__name__)
 
 
-INTERFACE = os.getenv("INTERFACE")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+INTERFACE      = os.getenv("INTERFACE")
+ACCESS_TOKEN   = os.getenv("ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
 
+# print(INTERFACE     )
+# print(ACCESS_TOKEN  )
+# print(CHANNEL_SECRET)
+
 handler = WebhookHandler(CHANNEL_SECRET)
+# line_bot_api = LineBotApi(ACCESS_TOKEN)
 configuration = Configuration(access_token=ACCESS_TOKEN)
 
 
@@ -51,48 +59,63 @@ def hello():
 
 @app.route('/welcome', methods=['GET'])
 def welcome():
-    name = request.args.get('name', 'guest')
-    print(name)
-    msg = f"Hello, {name.upper()}!!"
-    return msg
+   name = request.args.get('name', 'guest')
+   print(name)
+   msg = f"Hello, {name.upper()}!!"
+   return msg
 
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+   return 'I\'m alive!'
 
+ 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+   # print('callback')
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+   # handle webhook body
+   try:
+      handler.handle(body, signature)
+   except InvalidSignatureError:
+      app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+      abort(400)
 
-    return 'OK'
-
+   return 'OK'
 
 @handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        #相手の送信した内容で条件分岐して回答を変数に代入
-        if event.message.text == 'ip':
-            ip_address = check_ip()
-            msg = 'パー'
-        else:
-            msg = 'ごめんね。\nまだ他のメッセージには対応してないよ'
+def handle_message(event):   
+   print('message is received')
+   print(event.message.text)
 
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=msg)]
-            )
-        )
+   log_file = "/home/ubuntu/line-bot/log.txt"
+   with open(log_file, 'a', encoding='utf-8') as f:
+      f.write(event.message.text + '\n')
+   
+   
+   with ApiClient(configuration) as api_client:
+      #相手の送信した内容で条件分岐して回答を変数に代入
+      if event.message.text == 'グー':
+         msg = 'パー'
+      elif event.message.text == 'チョキ':
+         msg = 'グー'
+      elif event.message.text == 'パー':
+         msg = 'チョキ'
+      else:
+         msg = 'ごめんね。\nまだ他のメッセージには対応してないよ'
 
+      line_bot_api = MessagingApi(api_client)
+      line_bot_api.reply_message_with_http_info(
+         ReplyMessageRequest(
+            reply_token=event.reply_token,
+            messages=[TextMessage(text=msg)]
+         )
+      )
+    
 if __name__ == "__main__":
     app.run()
